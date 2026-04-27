6ae5c1b68225f15bf7f5ffa76817be019d79f697
Nikhil Singh <nikhilrajpu236@gmail.com>
2026-04-27 18:42:25 +0530

    Changing middleware

--- middleware.js
+++ middleware.js
@@ -1,26 +1,17 @@
 import { NextResponse } from "next/server";
 
-export function middleware(request) {
-  const token = request.cookies.get("refreshToken")?.value;
-  const { pathname } = request.nextUrl;
+export function middleware(req) {
+  const hasToken = req.cookies.get("refreshToken");
 
-  if (pathname.startsWith("/homepage")) {
-    if (!token) {
-      return NextResponse.redirect(
-        new URL("/authentication/signIn", request.url),
-      );
-    }
+  if (!hasToken && req.nextUrl.pathname.startsWith("/homepage")) {
+    console.log("failed");
+    return NextResponse.redirect(new URL("/authentication/signIn", req.url));
   }
-
-  if (pathname.startsWith("/authentication/signIn")) {
-    if (token) {
-      return NextResponse.redirect(new URL("/homepage", request.url));
-    }
-  }
+  console.log(hasToken);
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/homepage/:path*", "/authentication/signIn"],
+  matcher: ["/homepage/:path*"],
 };
