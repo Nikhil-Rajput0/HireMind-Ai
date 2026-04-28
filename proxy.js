7195090f70a6dc07fb63148d4d7afd29276bc68e
Nikhil Singh <nikhilrajpu236@gmail.com>
2026-04-28 21:24:56 +0530

    Changing middleware with proxy

--- proxy.js
+++ proxy.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export function middleware(request) {
+export function proxy(request) {
   const isLoggedIn = request.cookies.get("isLoggedIn")?.value;
   const { pathname } = request.nextUrl;
 
