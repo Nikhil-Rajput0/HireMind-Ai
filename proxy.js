eaa0dddd45b7cbba8086e6bdd0a313aa3fd2641f
Nikhil Singh <nikhilrajpu236@gmail.com>
2026-04-28 21:41:54 +0530

    Removing all bugs caused by proxy

--- proxy.js
+++ proxy.js
@@ -1,26 +1,23 @@
 import { NextResponse } from "next/server";
 
 export function proxy(request) {
-  const isLoggedIn = request.cookies.get("isLoggedIn")?.value;
-  const { pathname } = request.nextUrl;
-
-  if (pathname.startsWith("/homepage")) {
-    if (!isLoggedIn) {
-      return NextResponse.redirect(
-        new URL("/authentication/signIn", request.url),
-      );
-    }
-  }
-
-  if (pathname.startsWith("/authentication")) {
-    if (isLoggedIn) {
-      return NextResponse.redirect(new URL("/homepage", request.url));
-    }
-  }
-
-  return NextResponse.next();
+  // const isLoggedIn = request.cookies.get("isLoggedIn")?.value;
+  // const { pathname } = request.nextUrl;
+  // if (pathname.startsWith("/homepage")) {
+  //   if (!isLoggedIn) {
+  //     return NextResponse.redirect(
+  //       new URL("/authentication/signIn", request.url),
+  //     );
+  //   }
+  // }
+  // if (pathname.startsWith("/authentication")) {
+  //   if (isLoggedIn) {
+  //     return NextResponse.redirect(new URL("/homepage", request.url));
+  //   }
+  // }
+  // return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/homepage/:path*", "/authentication/:path*"],
+  // matcher: ["/homepage/:path*", "/authentication/:path*"],
 };
